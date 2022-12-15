e1bca972b87214ca3e7211e9e3bf0043b8603666
nicofishman <nicourifishman@gmail.com>
2022-12-15 15:34:11 -0300

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,tsx}",
+        "./pages/**/*.{html,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                neutral: {
+                    0: '#FFFFFF',
+                    100: '#F5F9FF',
+                    200: '#E6EDF7',
+                    300: '#252F3D',
+                    500: '#8FA3BF',
+                    600: '#7C899C',
+                    900: '#252F3D'
+                },
+                green: {
+                    default: '#29CC74',
+                    light: '#CCFFE3'
+                },
+                red: {
+                    default: '#E07F4F',
+                    light: '#FFDFD9'
+                },
+                brand: {
+                    light: '#E5F0FF',
+                    "light-2": '#CCE1FF',
+                }
+            },
+            backgroundImage: {
+                brandGradient: 'linear-gradient(102.47deg, #176FEB -5.34%, #FF80FF 106.58%)',
+                brandGradientHover: 'linear-gradient(102.47deg, #1667D9 -5.34%, #F279F2 106.58%)',
+                illustration: 'linear-gradient(102.47deg, #7296EB -5.34%, #EAC0E9 106.58%, #EAC0E9 106.58%)',
+                section: 'linear-gradient(102.47deg, rgba(23, 111, 235, 0.5) -5.34%, rgba(255, 128, 255, 0.5) 106.58%)',
+                aerolab: 'linear-gradient(180deg, #FF8800 0%, #FF6600 100%)'
+            },
+            fontFamily: {
+                monserrat: ['Montserrat', 'sans-serif'],
+            },
+            letterSpacing: {
+                24: '24%',
+            },
+            height: {
+                hug: '128px'
+            },
+            width: {
+                hug: '128px'
+            }
+        },
+    },
+    plugins: [],
+};
