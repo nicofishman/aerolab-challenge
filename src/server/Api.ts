00925299946b5fb374d33d1a10b2ea6a5f7603b9
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-02-16 11:35:06 -0300

    update env variables

--- src/server/Api.ts
+++ src/server/Api.ts
@@ -1,79 +1,80 @@
-import { Product, User } from "../types";
-
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzliMjdkODU2NDAxYTAwMjEwZjdjNDQiLCJpYXQiOjE2NzExMTI2NjR9.HSkanOptnbCCg6e6ijriW6ml1ebR0JIJRbmHJ2h6XEI';
-const BASE_URL = 'https://private-anon-c6b7f9f057-aerolabchallenge.apiary-proxy.com';
+import { Product, User } from '../types';
 
 export const getMe = async () => {
-    const response = await fetch(`${BASE_URL}/user/me`, {
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/user/me`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+            },
         }
-    });
-    const data = await response.json() as User;
-
-
+    );
+    const data = (await response.json()) as User;
 
     return data;
 };
 
-
 export const addPoints = async (points: number) => {
-    const response = await fetch(`${BASE_URL}/user/points`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ amount: points })
-    });
-    const data = await response.json() as {
-        message: string; "new Points": number;
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/user/points`,
+        {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: points }),
+        }
+    );
+    const data = (await response.json()) as {
+        message: string;
+        'new Points': number;
     };
 
     return data;
 };
 
 export const getProducts = async () => {
-    const response = await fetch(`${BASE_URL}/products`, {
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/products`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+            },
         }
-    });
-    const data = await response.json() as Product[];
+    );
+    const data = (await response.json()) as Product[];
 
     return data;
 };
 
 export const redeemProduct = async (productId: string) => {
-    const response = await fetch(`${BASE_URL}/redeem`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/redeem`, {
         method: 'POST',
         headers: {
-            Authorization: `Bearer ${API_KEY}`,
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
             'Content-Type': 'application/json',
-            Accept: 'application/json'
+            Accept: 'application/json',
         },
         body: JSON.stringify({
-            productId
-        })
+            productId,
+        }),
     });
 
     if (!response.ok) {
-        const json = await response.json() as { error: string };
-
-        console.log(json);
-
+        const json = (await response.json()) as { error: string };
 
         const data = {
             status: response.status,
-            message: json.error
-        } as { status: number, message: string };
+            message: json.error,
+        } as { status: number; message: string };
 
         return data;
     }
 
     const data = {
         status: response.status,
-        ...(await response.json())
+        ...(await response.json()),
     } as {
         status: number;
         message: string;
